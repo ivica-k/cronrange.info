916d9cd771b84833549ba616a98617cf8f8ccbfa
ivica-k <ivica.kolenkas@gmail.com>
2018-12-16 14:15:15 +0100

    travis ci integration

---
+++ deployment/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region                  = "eu-central-1"
+  profile                 = "default"
+  version                 = "1.54"
+}
+
+resource "aws_s3_bucket" "cronrange_bucket" {
+  bucket = "cronrange-bucket"
+  acl    = "public-read"
+  policy = "${file("s3_policy.json")}"
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+}
+
+output "website_url" {
+  value = "${aws_s3_bucket.cronrange_bucket.website_endpoint}"
+}
