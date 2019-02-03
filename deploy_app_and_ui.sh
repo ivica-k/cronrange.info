87aba3748700bc34f435f216be93da0ec5e1a623
ivica-k <ivica.kolenkas@gmail.com>
2019-02-03 17:12:11 +0100

    fixes a bug with chalice deployment using --stage correctly

--- deploy_app_and_ui.sh
+++ deploy_app_and_ui.sh
@@ -7,9 +7,8 @@ venv/bin/pip install awscli
 test "$TRAVIS_BRANCH" = "master" && export ENV="prod" || export ENV="dev"
 
 ./venv/bin/chalice deploy --stage "$ENV"
-CHALICE_URL=$(./venv/bin/chalice url)
-
-sed -i "s|http://localhost:8000/|$CHALICE_URL/|g" ./ui/index.html
+CHALICE_URL=$(./venv/bin/chalice url --stage "$ENV")
+sed -i "s|http://localhost:8000/|$CHALICE_URL|g" ./ui/index.html
 
 if [[ "${ENV}" = "dev" ]]; then
     ./venv/bin/aws s3 cp ./ui/. s3://dev.cronrange.info/ --recursive
