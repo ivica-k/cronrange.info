414aecfc8c5847d88fe0ded89ad6967174105fe9
ivica-k <ivica.kolenkas@gmail.com>
2019-02-03 18:58:21 +0100

    fixes the URL in js file

--- deploy_app_and_ui.sh
+++ deploy_app_and_ui.sh
@@ -4,12 +4,12 @@ test -d venv || virtualenv -p python3 venv
 venv/bin/pip install -Ur requirements.txt
 venv/bin/pip install awscli
 
-test "$TRAVIS_BRANCH" = "master" && export ENV="prod" || export ENV="dev"
+test "${TRAVIS_BRANCH}" = "master" && export ENV="prod" || export ENV="dev"
 
-./venv/bin/chalice deploy --stage "$ENV"
-CHALICE_URL=$(./venv/bin/chalice url)
+./venv/bin/chalice deploy --stage "${ENV}"
+CHALICE_URL=$(./venv/bin/chalice url --stage "${ENV}")
 
-sed -i "s|http://localhost:8000/|$CHALICE_URL/|g" ./ui/index.html
+sed -i "s|http://localhost:8000/|$CHALICE_URL/|g" ./ui/js/main.min.js
 
 if [[ "${ENV}" = "dev" ]]; then
     ./venv/bin/aws s3 cp ./ui/. s3://dev.cronrange.info/ --recursive
