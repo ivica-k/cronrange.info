ed823baa90f85e71f57349f1bff63ab7da4ce640
ivica-k <ivica.kolenkas@gmail.com>
2021-11-28 20:16:49 +0100

    chore: updates infra code and versions

---
+++ infra/dev/variables.tf
@@ -0,0 +1,14 @@
+variable "project" {
+  type    = string
+  default = "cronrange"
+}
+
+variable "domain" {
+  type    = string
+  default = "cronrange.info"
+}
+
+variable "env" {
+  type    = string
+  default = "dev"
+}
