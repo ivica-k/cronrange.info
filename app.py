a2c16a0350490f8f5e5cfe82f81074a84896ddf4
ivica-k <ivica.kolenkas@gmail.com>
2023-03-12 19:33:11 +0100

    feat: uses lambda powertools for better logging

--- app.py
+++ app.py
@@ -1,67 +1,70 @@
 import os
-import logging
 
 from chalicelib import get_cron_range
-from chalice import Chalice, Response, BadRequestError
+from chalice import Chalice, Response, BadRequestError, ConvertToMiddleware
 from datetime import datetime
 
-app = Chalice(app_name="cron_range")
+from aws_lambda_powertools import Logger
 
-log_format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-app.log.setLevel(os.getenv("LOG_LEVEL", "DEBUG"))
-
-for handler in app.log.handlers:
-	handler.setFormatter(log_format)
-
+ENV = os.getenv("ENV", "dev").lower()
 MAX_EXECUTIONS = int(os.getenv("MAX_EXECUTIONS", 100))
+
+app = Chalice(app_name=f"cron_range_{ENV}")
+logger = Logger()
+app.register_middleware(ConvertToMiddleware(logger.inject_lambda_context))
 
 
 @app.route("/", methods=["POST"], content_types=["application/json"], cors=True)
 def index_handler():
-	try:
-		request_json = app.current_request.json_body
-		cron = request_json["cron"]
-		executions = int(request_json.get("executions", 10))
-		date_time = request_json.get("datetime", datetime.now())
+    try:
+        request_json = app.current_request.json_body
+        cron = request_json["cron"]
+        executions = int(request_json.get("executions", 10))
+        date_time = request_json.get("datetime", datetime.now())
 
-		app.log.info(f"Got a request with cron: '{cron}'; Executions: '{executions}'; Datetime '{date_time}'")
+        logger.info(
+            f"Got a request with cron: '{cron}'; Executions: '{executions}'; Datetime '{date_time}'"
+        )
 
-		if executions > MAX_EXECUTIONS:
-			app.log.debug(f"Number of executions '{executions}' is greater than"
-			              f" '{MAX_EXECUTIONS}'. Defaulting to 100 executions")
-			executions = MAX_EXECUTIONS
+        if executions > MAX_EXECUTIONS:
+            logger.debug(
+                f"Number of executions '{executions}' is greater than"
+                f" '{MAX_EXECUTIONS}'. Defaulting to 100 executions"
+            )
+            executions = MAX_EXECUTIONS
 
-		ranges = get_cron_range(
-			num_items=executions,
-			cron_expression=cron,
-			start_datetime=date_time
-		)
+        ranges = get_cron_range(
+            num_items=executions, cron_expression=cron, start_datetime=date_time
+        )
 
-		return Response(
-			status_code=200,
-			body={"cron_ranges": ranges}
-		)
+        return Response(status_code=200, body={"cron_ranges": ranges})
 
-	except KeyError:
-		return Response(
-			status_code=400,
-			body={"message": "Make sure that JSON contains 'cron' and 'executions' key:value pairs"}
-		)
+    except KeyError:
+        message = "Make sure that JSON contains 'cron' and 'executions' key:value pairs"
+        logger.error(message)
 
-	except ValueError:
-		return Response(
-			status_code=400,
-			body={"message": "Field 'executions' must be a number"}
-		)
+        return Response(
+            status_code=400,
+            body={
+                "message": message
+            },
+        )
 
-	except BadRequestError:
-		return Response(
-			status_code=400,
-			body={"message": "Malformed JSON"}
-		)
+    except ValueError:
+        message = "Field 'executions' must be a number"
+        logger.error(message)
 
-	except Exception as ex:
-		return Response(
-			status_code=400,
-			body={"message": str(ex)}
-		)
+        return Response(
+            status_code=400, body={"message": message}
+        )
+
+    except BadRequestError:
+        message = "Malformed JSON"
+        logger.error(message)
+
+        return Response(status_code=400, body={"message": message})
+
+    except Exception as ex:
+        logger.error(f"Unknown error: {str(ex)}")
+
+        return Response(status_code=400, body={"message": "Unknown error"})
